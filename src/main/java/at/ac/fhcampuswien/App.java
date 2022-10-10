2eb9ad068b865eff53697d33f442cab9d078f161
c.hoerhan <c.hoerhan@htlstp.at>
2022-10-10 10:58:39 +0200

    Finish

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -72,6 +72,65 @@ public class App {
     }
 
     public static int randomNumberBetweenOneAndHundred(){
-        return new Random().nextInt(101);
+        return new Random().nextInt(100) + 1;
+    }
+
+    public static boolean swapArrays(int[] a, int[] b){
+        if(a.length != b.length){
+            return false;
+        }
+        int temp;
+
+        for(int i=0; i<a.length; i++){
+            temp = a[i];
+            a[i] = b[i];
+            b[i] = temp;
+        }
+        return true;
+    }
+
+    public static String camelCase(String s){
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int currentAsci, toAdd = 0;
+
+        if(chars[0] >= 'a' && chars[0] <= 'z'){
+            sb.append((char)(chars[0]-32));
+        } else if(chars[0] >= 'A' && chars[0] <= 'Z'){
+            sb.append(chars[0]);
+        }
+
+        for(int i=1; i<chars.length; i++){
+            currentAsci = (int)chars[i];
+            if((currentAsci >= 65 && currentAsci <= 90) // A-Z
+            || (currentAsci >= 97 && currentAsci <= 122)){ // a-z
+                if(chars[i-1] == ' '){ // Buchstabe groß
+                    if(chars[i] >= 'a'){
+                        currentAsci -= 32;
+                    }
+                }else if(currentAsci <= 90){ // sonst klein
+                    currentAsci += 32;
+                }
+                sb.append((char)currentAsci);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static int checkDigit(int[] code){
+        int sum = 0;
+        for(int i=0; i<code.length; i++){
+            sum += code[i]*(i+2);
+        }
+        int rest = sum % 11;
+        int check = 11 - rest;
+
+        if(check == 10){
+            check = 0;
+        } else if(check == 11){
+            check = 5;
+        }
+        return check;
     }
 }
